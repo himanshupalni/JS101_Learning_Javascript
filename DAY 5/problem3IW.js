4bcc83fdb676be46758f410677ec336cff403245
himanshu <himanshupalni61@gmail.com>
2022-10-13 16:23:49 +0530

    new file

---
+++ DAY 5/problem3IW.js
@@ -0,0 +1,7 @@
+let char = "b";
+if (char=="b" || char=="c" || char=="d" || char=="f" || char=="g" || char=="h" || char=="j" || char=="k"|| char=="l"|| char=="c" || char=="c" || char=="c" || char=="c"|| char=="c"|| char=="m" || char=="n" || char=="p" || char=="q"|| char=="r"|| char=="s" || char=="t" || char=="v" || char=="w"|| char=="x"|| char=="y" || char=="z"){
+  console.log("constant");
+
+}else{
+  console.log("vowel");
+}
